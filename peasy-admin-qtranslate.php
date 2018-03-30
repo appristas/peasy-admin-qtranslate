8c08af9a553bfaaefb8b5defb24efe4b5137bc42
Gasim Gasimzada <gasim@appristas.io>
2018-03-31 03:22:10 +0400

    Initial commit

---
+++ peasy-admin-qtranslate.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: Peasy Admin: qTranslate
+ * Plugin URI: https://github.com/appristas/peasy-admin-qtranslate
+ * Description: Peasy Admin qTranslate Translateable texts
+ * Version: 1.0.0
+ * Author: Appristas LLC
+ * Author URI: www.appristas.io
+ */
+
+defined( 'ABSPATH' ) or die( 'Tacita' );
+
+add_action( 'peasy_init', function() {
+	
+	
+} );

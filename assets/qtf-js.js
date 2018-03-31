852e52359cba52ead83357546846bbe1002540d2
Gasim Gasimzada <gasim@appristas.io>
2018-03-31 05:44:06 +0400

    When changing language buttons in one field will change buttons in all fields

--- assets/qtf-js.js
+++ assets/qtf-js.js
@@ -1,11 +1,10 @@
 (function($) {
-
     $(document).ready(function() {
         $('.qtf-language-button').click(function(e) {
             e.preventDefault();
-            $('.qtf-language-button').removeClass('is-current');
-            $(this).addClass('is-current');
             var lang = $(this).data('language');
+            $('.qtf-language-button').removeClass('is-current');
+            $('.qtf-language-button[data-language=' + lang + ']').addClass('is-current');
             $('.qtf-input').removeClass('is-current');
             $('.qtf-input[data-language=' + lang + ']').addClass('is-current');
         });

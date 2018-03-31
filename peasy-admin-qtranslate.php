9a0fd627c81219a7c11d4a23283deedcd407d482
Gasim Gasimzada <gasim@appristas.io>
2018-03-31 05:44:35 +0400

    Added admin notice error if qTranslate is not active

--- peasy-admin-qtranslate.php
+++ peasy-admin-qtranslate.php
@@ -10,7 +10,22 @@
 
 defined( 'ABSPATH' ) or die( 'Tacita' );
 
+add_action( 'admin_init', function() {
+	if ( ! function_exists( 'qtranxf_init_language' ) ) {
+		add_action( 'admin_notices', function() {
+		?>
+		<div class="notice notice-warning is-dismissible">
+	        <p><?php esc_html_e( 'Warning: qTranslate plugin is not active. This Peasy Admin Qtranslate will not work propely without qTranslate.', 'peasy-admin-qtranslate' ); ?></p>
+	    </div>
+		<?php
+		} );
+	}
+} );
+
 add_action( 'peasy_register', function() {
+	if ( ! function_exists( 'qtranxf_init_language' ) ) {
+		return;
+	}
 	require_once( __DIR__ . '/classes/class-translatabletextfield.php' );
 	\PeasyAdmin\PluggableFields::register_field( 'translatable_text', PeasyAdminQtranslate\TranslatableTextField::class );
 } );
